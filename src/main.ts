0dbed9498750eb6ee3a72d789d58650ff4ca8712
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2026-04-13 16:07:02 +0600

    Feat: Stripe Payment Module implemented

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
-const server = express();
-
-export const createServer = async () => {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors();
-  await app.init();
-  app.listen(process.env.PORT || 3000);
-  return server;
-};
-
-// Vercel expects a default export for the handler
-export default async (req: any, res: any) => {
-  await createServer();
-  server(req, res);
-};
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
